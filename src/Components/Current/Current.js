71774948e8b02ab8e18dfc102c4199f0a8e6985f
Peter Schmucki <pebesch@gmail.com>
2018-08-17 16:28:20 +0200

    Adding a current view

--- src/Components/Current/Current.js
+++ src/Components/Current/Current.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './Current.css';
 
 class Current extends React.Component {
   render() {
-    return <h1>Hallo!</h1>
+    const { current } = this.props
+    if ( !(current && current.condition) ) return <span>Loading</span>;
+
+    return (
+      <div className="Current">
+        <div className="Important">
+          <h1>{this.props.location.name}, {this.props.location.country} <span>{this.props.location.localtime}</span></h1>
+          <div className="QuickLook">
+            <div className="Flex">
+              <img src={this.props.current.condition.icon} alt={this.props.current.condition.text} />
+              <p>{this.props.current.temp_c}°C</p>
+            </div>
+            <p>Feels like {this.props.current.feelslike_c}°C</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
